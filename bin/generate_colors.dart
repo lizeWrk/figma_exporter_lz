e7540e2eaa5579131547daaa1b8e2cf1ac8d9a35
lizeWrk <lize@agmostudio.com>
2024-11-25 02:32:58 +0800

    feat: test

--- bin/generate_colors.dart
+++ bin/generate_colors.dart
@@ -1,18 +1,19 @@
 import 'package:figma_exporter/figma_exporter.dart';
 
 Future<void> main() async {
-  final folderPath = './scripts/colors';
-  final outputFilePath = './lib/app/assets/figmaColors.enum.dart';
+  // final folderPath = './scripts/colors';
+  // final outputFilePath = './lib/app/assets/figmaColors.enum.dart';
 
-  final generator = FigmaColorsEnumGenerator(
-    folderPath: folderPath,
-    outputFilePath: outputFilePath,
-  );
+  // final generator = FigmaColorsEnumGenerator(
+  //   folderPath: folderPath,
+  //   outputFilePath: outputFilePath,
+  // );
 
-  try {
-    await generator.generate();
-    print('Figma colors generated successfully.');
-  } catch (e) {
-    print('Error: $e');
-  }
+  // try {
+  //   await generator.generate();
+  //   print('Figma colors generated successfully.');
+  // } catch (e) {
+  //   print('Error: $e');
+  // }
+  print('123');
 }
